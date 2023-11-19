9873e8b81016af166f7f2aef10a19494a85eec80
donald.osgood@phrehab.com <donald.osgood@phrehab.com>
2023-11-18 21:01:14 -0600

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+''' 
+@Program: admin
+@Author: Donald Osgood
+@Last Date: 2023-11-18 19:14:16
+@Purpose:Donald Osgood
+'''
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
